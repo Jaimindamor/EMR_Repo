fa8e5d82f2c002c4261c0eb411e02391228eaeeb
JAIMIN DAMOR <jaimindamor001@gmail.com>
2024-03-04 14:56:41 +0530

    changes

---
+++ EMR/EMR_App/custompermission.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class Mypermission(BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Nurse's").exists():
+            return True
+
+class doctorpermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Doctor's").exists():
+            return True
